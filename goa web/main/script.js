f6bb012af6d60fd0480a2d414ba04595944ad78d
SabaBok <saba.bokuchava555@gmail.com>
2025-04-27 00:06:03 +0400

    commit

--- goa web/main/script.js
+++ goa web/main/script.js
@@ -7,3 +7,14 @@ syl.addEventListener("mouseover",function(){
 syl.addEventListener("mouseout",function(){
     idk.style.display = "none"
 })
+
+
+let p = document.querySelector(".soci")
+let socis = document.querySelector(".drop-socials")
+let kkk = document.querySelector(".socials")
+p.addEventListener("mouseover",function(){
+    socis.style.display = "block"
+    socis.addEventListener("mouseout",function(){
+        this.style.display = "none"
+    })
+})
