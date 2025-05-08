33d741fd0827fe07cd4c55e6b67ef1c32568c7d0
SabaBok <saba.bokuchava555@gmail.com>
2025-05-08 19:47:44 +0400

    commit

--- goa-web/Important-Vids/imp.js
+++ goa-web/Important-Vids/imp.js
@@ -20,3 +20,50 @@ social.addEventListener("mouseout", ()=>{
     social.style.height = ""
     socDrop.style.display = "none"
 })
+
+
+let allSylabExpand = document.querySelector(".hidden-sylab-expand")
+let sylabShower = document.querySelector(".side-syl")
+let hidenSylab = document.querySelector(".hidden-sylab")
+let allSylabsShown = false
+sylabShower.addEventListener("click", e =>{
+    if(allSylabsShown == false){
+        allSylabExpand.style.height = "auto"
+        allSylabsShown = true;
+        hidenSylab.style.backgroundColor = "#ffffff36"
+    }else{
+        allSylabExpand.style.height = "0px"
+        allSylabsShown = false
+        hidenSylab.style.backgroundColor = "transparent"
+    }
+})
+
+
+let sylCard = document.querySelectorAll(".syl-card")
+for(let i of sylCard){
+    let div = i.querySelector("div")
+    let h4 = i.querySelector("h4")
+    let debounce = false 
+    h4.addEventListener("click", e =>{
+        if(debounce == false){
+            div.style.height = "auto"
+            debounce = true
+            i.style.backgroundColor = "#ffffff3f"
+        }else{
+            div.style.height = "0px"
+            debounce = false
+            i.style.backgroundColor = "transparent"
+        }
+    })
+}
+
+
+let burger = document.querySelector(".burger")
+let sideBar = document.querySelector("#side-bar")   
+burger.addEventListener("click", e =>{
+    sideBar.style.transform = "translateX(0px)"
+})
+let escaper = document.querySelector(".escape")  
+escaper.addEventListener("click", e =>{
+    sideBar.style.transform = "translateX(100%)"
+})
